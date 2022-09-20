71365e97fcc89239731a063a3f676ee525943d4e
Drew Miller <drew.miller@mrjaffesclass.com>
2022-09-20 11:45:00 -0700

    added arrays

--- Operators.java
+++ Operators.java
@@ -16,4 +16,13 @@ public class Operators
         string1.equals(string2);
         String string3 = "I am also a string";
         boolean stringsEquals = string1.compareTo(string3);
+        String[] strArray = new String[5];
+        String[] strArray = {"Alpha", "Bravo", "Charlie", "Delta", "Echo"};
+        for (String a = 0; i < myArray.length; a++) {
+            System.out.println(myArray[a]);
+        }
+        for (String value : myArray) {
+            System.out.print(value);
+        }
+        
 }
