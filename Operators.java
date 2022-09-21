83877b2928f3e068084331cf693c081efe7ddaa7
Drew Miller <drew.miller@mrjaffesclass.com>
2022-09-21 11:49:08 -0700

    Arrays

--- Operators.java
+++ Operators.java
@@ -12,17 +12,24 @@ public class Operators
         System.out.print(f);
         System.out.print(flag);
         String string1 = "I am a string";
-        String string2;
+        String string2 = string1;
         string1.equals(string2);
         String string3 = "I am also a string";
-        boolean stringsEquals = string1.compareTo(string3);
-        String[] strArray = new String[5];
-        String[] strArray = {"Alpha", "Bravo", "Charlie", "Delta", "Echo"};
-        for (String a = 0; i < myArray.length; a++) {
-            System.out.println(myArray[a]);
+        boolean stringsEquals = string1.equals(string3);
+        String[] strArray = {"Alpha","Bravo","Charlie","Delta","Echo"};
+        for (int a = 0; a < strArray.length; a++) {
+            System.out.println(strArray[a]);
         }
-        for (String value : myArray) {
+        for (String value : strArray) {
             System.out.print(value);
         }
-        
+        System.out.println();
+        for (int a = 0; a < strArray.length; ++a) {
+            System.out.print(strArray[a]);
+            if (a < strArray.length-1) {
+                System.out.print("-");
+            }
+        }
+
+}
 }
